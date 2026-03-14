c0913cb1b85edd45befcb01fef9be564db745dad
Duong Phu Dong <dongduong840@gmail.com>
2026-03-14 12:14:56 +0700

    fix(sitemap): update URL to the correct deployment link

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://duongphudong.com',
+            url: 'https://duongphudong2001.vercel.app/',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 1,
